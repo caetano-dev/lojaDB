6df8184827ed460452952e22151f8f252cf8a622
Drull <pcaetanop@protonmail.com>
2023-11-10 19:55:08 -0300

    Order and attributes

--- main.sql
+++ main.sql
@@ -1,9 +1,9 @@
 
 -- Database: loja
 
-DROP DATABASE IF EXISTS test;
+DROP DATABASE IF EXISTS loja;
 
-CREATE DATABASE test
+CREATE DATABASE loja
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -12,67 +12,7 @@ CREATE DATABASE test
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False;
-
--- Table: public.funcionario
-
-DROP TABLE IF EXISTS public.funcionario;
-
-CREATE TABLE IF NOT EXISTS public.funcionario
-(
-    "CPFfuncionario" numeric NOT NULL,
-    nome character varying NOT NULL,
-    cargo character varying NOT NULL,
-    telefone numeric NOT NULL,
-    email character varying NOT NULL,
-    salario numeric NOT NULL,
-    CONSTRAINT funcionario_pkey PRIMARY KEY ("CPFfuncionario"),
-    CONSTRAINT telefone UNIQUE (telefone)
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.funcionario
-    OWNER to postgres;
-
----
-
--- Table: public.cliente
-
-DROP TABLE IF EXISTS public.cliente;
-
-CREATE TABLE IF NOT EXISTS public.cliente
-(
-    nome character varying NOT NULL,
-    email character varying NOT NULL,
-    endereco character varying NOT NULL,
-    "CPFcliente" numeric NOT NULL,
-    CONSTRAINT "CPFcliente" PRIMARY KEY ("CPFcliente")
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.cliente
-    OWNER to postgres;
-
-
--- Table: public.fornecedor
-
-DROP TABLE IF EXISTS public.fornecedor;
-
-CREATE TABLE IF NOT EXISTS public.fornecedor
-(
-    "CNPJfornecedor" numeric NOT NULL,
-    CONSTRAINT "CNPJfornecedor" PRIMARY KEY ("CNPJfornecedor")
-)
-
-TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS public.fornecedor
-    OWNER to postgres;
-
-
 -- Table: public.produto
-
 DROP TABLE IF EXISTS public.produto;
 
 CREATE TABLE IF NOT EXISTS public.produto
@@ -82,18 +22,52 @@ CREATE TABLE IF NOT EXISTS public.produto
     marca character varying NOT NULL,
     tipo character varying NOT NULL,
     preco numeric NOT NULL,
-    "quantidadeEmEstoque" integer NOT NULL,
+    quantidade integer NOT NULL,
     CONSTRAINT produto_pkey PRIMARY KEY ("IDproduto")
 )
-
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.produto
     OWNER to postgres;
 
 
--- Table: public.venda
+-- Table: public.cliente
+DROP TABLE IF EXISTS public.cliente;
 
+CREATE TABLE IF NOT EXISTS public.cliente
+(
+    nome character varying NOT NULL,
+    email character varying NOT NULL,
+    endereco character varying NOT NULL,
+    "CPFcliente" numeric NOT NULL,
+    telefone numeric NOT NULL,
+    CONSTRAINT "CPFcliente" PRIMARY KEY ("CPFcliente"),
+    CONSTRAINT telefone UNIQUE (telefone)
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.cliente
+    OWNER to postgres;
+
+
+-- Table: public.vendedor
+DROP TABLE IF EXISTS public.vendedor;
+
+CREATE TABLE IF NOT EXISTS public.vendedor
+(
+    "CPFvendedor" numeric NOT NULL,
+    nome character varying NOT NULL,
+    email character varying NOT NULL,
+    salario numeric NOT NULL,
+    CONSTRAINT vendedor_pkey PRIMARY KEY ("CPFvendedor")
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.vendedor
+    OWNER to postgres;
+
+
+-- Table: public.venda
 DROP TABLE IF EXISTS public.venda;
 
 CREATE TABLE IF NOT EXISTS public.venda
@@ -101,7 +75,7 @@ CREATE TABLE IF NOT EXISTS public.venda
     "IDvenda" integer NOT NULL,
     "dataVenda" date NOT NULL,
     "valorTotal" numeric NOT NULL,
-    "CPFfuncionario" numeric NOT NULL,
+    "CPFvendedor" numeric NOT NULL,
     "CPFcliente" numeric NOT NULL,
     CONSTRAINT venda_pkey PRIMARY KEY ("IDvenda"),
     CONSTRAINT "CPFcliente" FOREIGN KEY ("CPFcliente")
@@ -109,20 +83,41 @@ CREATE TABLE IF NOT EXISTS public.venda
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID,
-    CONSTRAINT "CPFfuncionario" FOREIGN KEY ("CPFfuncionario")
-        REFERENCES public.funcionario ("CPFfuncionario") MATCH SIMPLE
+    CONSTRAINT "CPFvendedor" FOREIGN KEY ("CPFvendedor")
+        REFERENCES public.vendedor ("CPFvendedor") MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
 )
-
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.venda
     OWNER to postgres;
 
--- Table: public.inclui
 
+-- Table: public.fornecedor
+DROP TABLE IF EXISTS public.fornecedor;
+
+CREATE TABLE IF NOT EXISTS public.fornecedor
+(
+    "CNPJfornecedor" numeric NOT NULL,
+    "ValorTotal" numeric NOT NULL,
+    "IDproduto" integer NOT NULL,
+    quantidade numeric NOT NULL,
+    CONSTRAINT "CNPJfornecedor" PRIMARY KEY ("CNPJfornecedor"),
+    CONSTRAINT "IDproduto" FOREIGN KEY ("IDproduto")
+        REFERENCES public.produto ("IDproduto") MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+)
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.fornecedor
+    OWNER to postgres;
+
+
+-- Table: public.inclui
 DROP TABLE IF EXISTS public.inclui;
 
 CREATE TABLE IF NOT EXISTS public.inclui
@@ -141,7 +136,6 @@ CREATE TABLE IF NOT EXISTS public.inclui
         ON DELETE NO ACTION
         NOT VALID
 )
-
 TABLESPACE pg_default;
 
 ALTER TABLE IF EXISTS public.inclui
